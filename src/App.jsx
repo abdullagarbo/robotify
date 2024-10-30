8b349d9b49e9bf3bc9d70553308542e8d51cbe8e
Abdulla <abdulla.garbo@gmail.com>
2024-10-30 15:49:53 +0100

    installed dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <a href='/#' target='_blank'>
+      <a href='https://www.astrazeneca.com' target='_blank'>
         <img src={robotLogo} className='logo' alt='Robotify logo' />
       </a>
       <h1>Robotify</h1>
